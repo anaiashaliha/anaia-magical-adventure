ddf210eec20cd2af685e27fe0770e54d0e2369ba
anaiashaliha <abdulbackup44@gmail.com>
2026-03-13 10:28:14 +0700

    Create js/config.js file for game configuration

---
+++ js/config.js
@@ -0,0 +1,17 @@
+// Game Configuration
+
+const CONFIG = {
+    game: {
+        title: 'Anaia\'s Magical Adventure',
+        version: '1.0.0',
+        targetAge: 5
+    },
+    canvas: {
+        width: 800,
+        height: 600
+    },
+    assets: {
+        imagePath: './assets/images/',
+        soundPath: './assets/sounds/'
+    }
+};
